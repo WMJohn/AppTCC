a23ec089748be6e6f37b95a63a19daacdb4edac8
W John <werner.mota@gmail.com>
2018-11-05 15:00:18 -0200

    medias linechart
    media foi redusido mapara media geral das obj e redação.
    iniciado testes com linechart

--- js/app.js
+++ js/app.js
@@ -4,11 +4,15 @@ var sexoChart = dc.pieChart("#sexoChart");
 var tipoEscolaChart = dc.pieChart("#tipoEscolaChart");
 var localEscolaChart = dc.pieChart("#localEscolaChart");
 //nota media
-var md_cnChart = dc.rowChart("#md_cnChart");
-var md_chChart = dc.rowChart("#md_chChart");
-var md_lcChart = dc.rowChart("#md_lcChart");
-var md_mtChart = dc.rowChart("#md_mtChart");
+//var md_cnChart = dc.rowChart("#md_cnChart");
+//var md_chChart = dc.rowChart("#md_chChart");
+//var md_lcChart = dc.rowChart("#md_lcChart");
+//var md_mtChart = dc.rowChart("#md_mtChart");
 var md_redChart = dc.rowChart("#md_redChart");
+var md_objChart = dc.rowChart("#md_objChart");
+//linechart
+var evoLineObjChart = dc.lineChart("#evoLineObjChart")
+var evoLineRedChart = dc.lineChart("#evoLineRedChart")
 
 var url = "base/enem-2009-16-escolas.csv";
 
@@ -18,7 +22,10 @@ var url = "base/enem-2009-16-escolas.csv";
  2009,11000058,RO,1100205,M,4,1,56,620.2,615.7,558.3,605.7,650.9
  2009,11000198,RO,1100205,F,4,1,28,601.7,610.2,577.8,559.9,646.4
  **/
-dc.config.defaultColors(d3.schemeSet1)
+
+// ifpa 15588947
+
+dc.config.defaultColors(d3.schemeSet2)
 
 d3.csv(url).then(function (data) {
 
@@ -48,7 +55,8 @@ d3.csv(url).then(function (data) {
                 }
                 return label;
             })
-            .ordinalColors(d3.schemeSet2);
+            .ordinalColors(d3.schemeCategory10)
+            .legend(dc.legend().x(10).y(10).itemHeight(13).gap(5))
 
 
 //    /**    Tipo de Escola **/
@@ -72,7 +80,8 @@ d3.csv(url).then(function (data) {
                 }
                 return label;
             })
-            .ordinalColors(d3.schemeSet2);
+            .ordinalColors(d3.schemeCategory10)
+            .legend(dc.legend().x(10).y(10).itemHeight(13).gap(5))
 
 
     /**  local da Escola **/
@@ -95,7 +104,8 @@ d3.csv(url).then(function (data) {
                 }
                 return label;
             })
-            .ordinalColors(d3.schemeSet2);
+            .ordinalColors(d3.schemeCategory10)
+            .legend(dc.legend().x(10).y(10).itemHeight(13).gap(5))
 
     /**    Numero de Participantes                       **/
 
@@ -104,8 +114,6 @@ d3.csv(url).then(function (data) {
         return d.inscritos;
     });
 
-    anoDimMax = anoGroup.top(1)[0].value;
-    console.log(anoDimMax)
 
     anoChart
             .width(300)
@@ -119,7 +127,7 @@ d3.csv(url).then(function (data) {
             .xAxisLabel("Ano")
             .dimension(anoDim)
             .group(anoGroup)
-            .ordinalColors(d3.schemeSet2);
+            .ordinalColors(d3.schemeCategory10);
 
 ////***      Numero de Participantes           por estado             ****//
 
@@ -139,108 +147,118 @@ d3.csv(url).then(function (data) {
             .xAxisLabel("UF")
             .dimension(numporestaDim)
             .group(numporestaGroup)
-            .ordinalColors(d3.schemeSet2);
+            .ordinalColors(d3.schemeCategory10);
+
+    /** **************  Percentual de participantes em relacao ao ano anterior  ******************** **/
+
+
+
 
     /** **************  Media  das Notas  ******************** **/
 
-    var md_cnGroup = anoDim.group().reduce(reduceCNAdd, reduceCNRemove, reduceCNInitial)
-    var md_chGroup = anoDim.group().reduce(reduceCHAdd, reduceCHRemove, reduceCHInitial);
-    var md_lcGroup = anoDim.group().reduce(reduceLCAdd, reduceLCRemove, reduceLCInitial);
-    var md_mtGroup = anoDim.group().reduce(reduceMTAdd, reduceMTRemove, reduceMTInitial);
+//    var md_cnGroup = anoDim.group().reduce(reduceCNAdd, reduceCNRemove, reduceCNInitial)
+//    var md_chGroup = anoDim.group().reduce(reduceCHAdd, reduceCHRemove, reduceCHInitial);
+//    var md_lcGroup = anoDim.group().reduce(reduceLCAdd, reduceLCRemove, reduceLCInitial);
+//    var md_mtGroup = anoDim.group().reduce(reduceMTAdd, reduceMTRemove, reduceMTInitial);
     var md_redGroup = anoDim.group().reduce(reduceREDAdd, reduceREDRemove, reduceREDInitial);
 
-//    var mediaObjDim = ndx.dimension(nota => {
-//        somaObj = (nota.md_cnDim * 1.0) + (nota.md_chDim * 1.0) + (nota.md_lcDim * 1.0) + (nota.md_mtDim * 1.0);
+//    var notaPorGrupoDim = ndx.dimension(nota => {
 //        
-//        if (somaObj <= 250) {
-//            return 'Grupo_1';
-//        } else if (somaObj <= 500) {
-//            return 'Grupo_2';
-//        } else if (somaObj <= 750) {
-//            return 'Grupo_3';
+//        somaObj = (parseFloat(nota.md_cn) + parseFloat(nota.md_ch) + parseFloat(nota.md_lc) + parseFloat(nota.md_mt)) / 4;
+//
+//        if (parseFloat(somaObj) <= 250) {
+//            return 'Grupo_1 <= 250';
+//        } else if (parseFloat(somaObj) <= 500) {
+//            return 'Grupo_2 <= 500';
+//        } else if (parseFloat(somaObj) <= 750) {
+//            return 'Grupo_3 <= 750';
 //        } else  {
-//            return 'Grupo_4' ;
+//            return 'Grupo_4 > 750' ;
 //        }
+//        
 //    });
-//    
-//     var mediaObjGroup = mediaObjDim.group().reduce(reduceMedObjAdd, reduceMedObjRemove, reduceMedObjInitial)
+//        
+//     var notaPorGrupoGroup =  notaPorGrupoDim.group().reduceSum(function (d) {
+//        return d.inscritos;
+//    });
+
+
+    var notaPorGrupoGroup = anoDim.group().reduce(reduceMedObjAdd, reduceMedObjRemove, reduceMedObjInitial)
 
 ////reduce media Provas OBjetivas  ---------------------------------------------------
-//    function reduceMedObjAdd(p, v) {
+    function reduceMedObjAdd(p, v) {
+        ++p.count;
+        p.total += (parseFloat(v.md_cn) + parseFloat(v.md_ch) + parseFloat(v.md_lc) + parseFloat(v.md_mt)) / 4;
+        return p;
+    }
+    function reduceMedObjRemove(p, v) {
+        --p.count;
+        p.total -= (parseFloat(v.md_cn) + parseFloat(v.md_ch) + parseFloat(v.md_lc) + parseFloat(v.md_mt)) / 4;
+        return p;
+    }
+    function reduceMedObjInitial() {
+        return {count: 0, total: 0};
+    }
+////reduce media CN  ---------------------------------------------------
+//    function reduceCNAdd(p, v) {
 //        ++p.count;
-//        p.total += (v.md_cnDim * 1.0) + (v.md_chDim * 1.0) + (v.md_lcDim * 1.0) + (v.md_mtDim * 1.0);
+//        p.total += parseFloat(v.md_cn);
 //        Math.round(p)
 //        return p;
 //    }
-//    function reduceMedObjRemove(p, v) {
+//    function reduceCNRemove(p, v) {
 //        --p.count;
-//        p.total -= (v.md_cnDim * 1.0) + (v.md_chDim * 1.0) + (v.md_lcDim * 1.0) + (v.md_mtDim * 1.0);
+//        p.total -= parseFloat(v.md_cn);
 //        return p;
 //    }
-//    function reduceMedObjInitial() {
+//    function reduceCNInitial() {
 //        return {count: 0, total: 0};
 //    }
-
-//reduce media CN  ---------------------------------------------------
-    function reduceCNAdd(p, v) {
-        ++p.count;
-        p.total += (v.md_cn) * 1;
-        Math.round(p)
-        return p;
-    }
-    function reduceCNRemove(p, v) {
-        --p.count;
-        p.total -= (v.md_cn) * 1;
-        return p;
-    }
-    function reduceCNInitial() {
-        return {count: 0, total: 0};
-    }
-//reduce media CH -----------------------------------------------
-    function reduceCHAdd(p, v) {
-        ++p.count;
-        p.total += (v.md_ch) * 1;
-        //console.log(p.total)
-        return p;
-    }
-    function reduceCHRemove(p, v) {
-        --p.count;
-        p.total -= (v.md_ch) * 1;
-        return p;
-    }
-    function reduceCHInitial() {
-        return {count: 0, total: 0};
-    }
-//reduce media LC ------------------------------------------------
-    function reduceLCAdd(p, v) {
-        ++p.count;
-        p.total += (v.md_lc) * 1;
-        //console.log(p.total)
-        return p;
-    }
-    function reduceLCRemove(p, v) {
-        --p.count;
-        p.total -= (v.md_lc) * 1;
-        return p;
-    }
-    function reduceLCInitial() {
-        return {count: 0, total: 0};
-    }
-//reduce media MT ------------------------------------------------
-    function reduceMTAdd(p, v) {
-        ++p.count;
-        p.total += (v.md_mt) * 1;
-        //console.log(p.total)
-        return p;
-    }
-    function reduceMTRemove(p, v) {
-        --p.count;
-        p.total -= (v.md_mt) * 1;
-        return p;
-    }
-    function reduceMTInitial() {
-        return {count: 0, total: 0};
-    }
+////reduce media CH -----------------------------------------------
+//    function reduceCHAdd(p, v) {
+//        ++p.count;
+//        p.total += parseFloat(v.md_ch);
+//        //console.log(p.total)
+//        return p;
+//    }
+//    function reduceCHRemove(p, v) {
+//        --p.count;
+//        p.total -= parseFloat(v.md_ch);
+//        return p;
+//    }
+//    function reduceCHInitial() {
+//        return {count: 0, total: 0};
+//    }
+////reduce media LC ------------------------------------------------
+//    function reduceLCAdd(p, v) {
+//        ++p.count;
+//        p.total += (v.md_lc) * 1;
+//        //console.log(p.total)
+//        return p;
+//    }
+//    function reduceLCRemove(p, v) {
+//        --p.count;
+//        p.total -= (v.md_lc) * 1;
+//        return p;
+//    }
+//    function reduceLCInitial() {
+//        return {count: 0, total: 0};
+//    }
+////reduce media MT ------------------------------------------------
+//    function reduceMTAdd(p, v) {
+//        ++p.count;
+//        p.total += (v.md_mt) * 1;
+//        //console.log(p.total)
+//        return p;
+//    }
+//    function reduceMTRemove(p, v) {
+//        --p.count;
+//        p.total -= (v.md_mt) * 1;
+//        return p;
+//    }
+//    function reduceMTInitial() {
+//        return {count: 0, total: 0};
+//    }
 //reduce media RED ------------------------------------------------
     function reduceREDAdd(p, v) {
         ++p.count;
@@ -257,64 +275,157 @@ d3.csv(url).then(function (data) {
         return {count: 0, total: 0};
     }
 
-    md_cnChart
-            .width(300)
-            .height(200)
-            .ordinalColors(d3.schemeSet2)
-            .dimension(anoDim)
-            .group(md_cnGroup, '1')
-            .x(d3.scaleOrdinal().domain([300, 1000]))
-            .elasticX(true)
-            .valueAccessor(function (p) {
-                return p.value.count > 0 ? p.value.total / p.value.count : 0;
-            });
-
-    md_chChart
-            .width(300)
-            .height(200)
-            .ordinalColors(d3.schemeSet2)
-            .dimension(anoDim)
-            .group(md_chGroup, '1')
-            .x(d3.scaleLinear().domain([300, 1000]))
-            .elasticX(true)
-            .valueAccessor(function (p) {
-                return p.value.count > 0 ? p.value.total / p.value.count : 0;
-            });
-    md_lcChart
-            .width(300)
-            .height(200)
-            .ordinalColors(d3.schemeSet2)
-            .dimension(anoDim)
-            .group(md_lcGroup, '1')
-            .x(d3.scaleLinear().domain([300, 1000]))
-            .elasticX(true)
-            .valueAccessor(function (p) {
-                return p.value.count > 0 ? p.value.total / p.value.count : 0;
-            });
-    md_mtChart
-            .width(450)
-            .height(200)
-            .ordinalColors(d3.schemeSet2)
-            .dimension(anoDim)
-            .group(md_mtGroup, '1')
-            .x(d3.scaleOrdinal().domain([300, 1000]))
-            .elasticX(true)
-            .valueAccessor(function (p) {
-                return p.value.count > 0 ? p.value.total / p.value.count : 0;
-            });
+//    md_cnChart
+//            .width(300)
+//            .height(200)
+//            .ordinalColors(d3.schemeSet2)
+//            .dimension(anoDim)
+//            .group(notaPorGrupoGroup)
+//            //.x(d3.scaleOrdinal().domain([0, maxPart]))
+//            .elasticX(true)
+//            .valueAccessor(function (p) {
+//                return p.value.count > 0 ? p.value.total / p.value.count : 0;
+//            })
+//    md_chChart
+//            .width(300)
+//            .height(200)
+//            .ordinalColors(d3.schemeSet2)
+//            .dimension(anoDim)
+//            .group(md_chGroup, '2')
+//            .x(d3.scaleLinear().domain([300, 1000]))
+//            .elasticX(true)
+//            .valueAccessor(function (p) {
+//                return p.value.count > 0 ? p.value.total / p.value.count : 0;
+//            });
+//    md_lcChart
+//            .width(300)
+//            .height(200)
+//            .ordinalColors(d3.schemeSet2)
+//            .dimension(anoDim)
+//            .group(md_lcGroup, '3')
+//            .x(d3.scaleLinear().domain([300, 1000]))
+//            .elasticX(true)
+//            .valueAccessor(function (p) {
+//                return p.value.count > 0 ? p.value.total / p.value.count : 0;
+//            });
+//    md_mtChart
+//            .width(450)
+//            .height(200)
+//            .ordinalColors(d3.schemeSet2)
+//            .dimension(anoDim)
+//            .group(md_mtGroup, '4')
+//            .x(d3.scaleOrdinal().domain([300, 1000]))
+//            .elasticX(true)
+//            .valueAccessor(function (p) {
+//                return p.value.count > 0 ? p.value.total / p.value.count : 0;
+//            });
     md_redChart
             .width(450)
             .height(200)
-            .ordinalColors(d3.schemeSet2)
+            .ordinalColors(d3.schemeCategory10)
             .dimension(anoDim)
-            .group(md_redGroup, '1')
+            .group(md_redGroup, '5')
             .x(d3.scaleLinear().domain(["teste"]))
             .elasticX(true)
             .valueAccessor(function (p) {
                 return p.value.count > 0 ? p.value.total / p.value.count : 0;
-            });
+            })
+            .label(function (d) {
+                if (localEscolaChart.hasFilter() && !localEscolaChart.hasFilter(d.key)) {
+                    return d.key + '(0%)';
+                }
+                var label = d.key;
+                if (all.value()) {
+                    label += ' (' + (d.value.total/d.value.count).toFixed(2)  + ')';
+                }
+                console.log(d.value)
+                return label;
+            })
+    md_objChart
+            .width(450)
+            .height(200)
+            .ordinalColors(d3.schemeCategory10)
+            .dimension(anoDim)
+            .group(notaPorGrupoGroup, '6')
+            .x(d3.scaleLinear().domain(["teste"]))
+            .elasticX(true)
+            .valueAccessor(function (p) {
+                return p.value.count > 0 ? p.value.total / p.value.count : 0;
+            })
+            .label(function (d) {
+                if (localEscolaChart.hasFilter() && !localEscolaChart.hasFilter(d.key)) {
+                    return d.key + '(0%)';
+                }
+                var label = d.key;
+                if (all.value()) {
+                    label += ' (' + (d.value.total/d.value.count).toFixed(2) + ')';
+                }
+                console.log(d.value)
+                return label;
+            })
 
 
-////    /**        Media Geral Nota Redacao         **/
+
+//// 
+
+    var percentGroup = anoDim.group().reduce(
+            function (p, v) {
+                ++p.count;
+                p.total += parseFloat(v.inscritos);
+                return p;
+            },
+            function (p, v) {
+                --p.count;
+                p.total -= parseFloat(v.inscritos);
+                return p;
+            },
+            function () {
+                return {
+                    count: 0,
+                    total: 0,
+                };
+            }
+    );
+
+    evoLineObjChart
+            .width(450)
+            .height(200)
+            .margins({top: 10, right: 20, bottom: 30, left: 50})
+            .x(d3.scaleOrdinal())
+            .elasticY(true)
+            .xUnits(dc.units.ordinal)
+            .yAxisLabel("Decay %")
+            .dimension(anoDim)
+            .valueAccessor(function (d) {
+                return (d.value.total/d.value.count)
+            })
+            .group(notaPorGrupoGroup)
+            .on('renderlet', function (chart) {
+                chart.selectAll('rect').on("click", function (d) {
+                    console.log("click!", d);
+                });
+            })
+
+    evoLineRedChart
+            .width(450)
+            .height(200)
+            .margins({top: 10, right: 20, bottom: 30, left: 50})
+            .x(d3.scaleOrdinal())
+            .elasticY(true)
+            .xUnits(dc.units.ordinal)
+            .yAxisLabel("Decay %")
+            .dimension(anoDim)
+            .valueAccessor(function (d) {
+                console.log(d)
+                return (d.value.total/d.value.count)
+            })
+            .group(notaPorGrupoGroup)
+            .on('renderlet', function (chart) {
+                chart.selectAll('rect').on("click", function (d) {
+                    console.log("click!", d);
+                });
+            })
+
+
     dc.renderAll();
 });
